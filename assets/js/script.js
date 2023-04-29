4edcc2cdcd6ff40136b35b82f8be05dda54699e2
aripandini <ariannepandini@hotmail.com>
2023-04-29 14:04:22 -0400

    added intro section, as well as moved the search bar to go with future layout of search results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,59 @@
 var searchBtn = $('#search'); 
 var searchBar = $('#searchBar');
 var booksList = $("#books");
-var bookColumns = $("#bookColumns"); 
+// var bookColumns = $("#bookColumns"); 
 var search = searchBar.textContent; 
 
+//Hides Intro on Search // added 
+searchBtn.click(function() {
+  $("#intro").hide(); 
+})
+
 searchBtn.on("click",function(){
-    booksList[0].textContent = ""; 
-    var search = $('#searchBar').val();
-    if (search == ''){
-        alert("Please enter something in the search bar")
-    }
-    else {
-       getBooks(search); 
-       
-    }
+  // booksList[0].textContent = ""; 
+  var search = $('#searchBar').val();
+  if (search == ''){
+      alert("Please enter something in the search bar")
+  }
+  else {
+     getBooks(search); 
+     
+  }
 });
 
+var table = document.getElementById("table");
 
 function getBooks(books){
   var encodedSearch = encodeURIComponent(books); 
-  var apiUrl = "https://www.googleapis.com/books/v1/volumes?q=" + encodedSearch + "&maxResults=10&key=AIzaSyB8K95K3Skp2q0fAnkEfjlwQpcQRrLLv5Y"; 
-  console.log(apiUrl)
+  var apiUrl = "https://www.googleapis.com/books/v1/volumes?q=" + encodedSearch + "&maxResults=10&key=AIzaSyB8K95K3Skp2q0fAnkEfjlwQpcQRrLLv5Y";  
   fetch(apiUrl)
-    .then((response) => {
-      return response.json()
-      })
-    .then(function(data){ 
-      console.log(data);
-      for (var i=0; i < data.items.length; i++){
-      var tr = document.createElement("tr");
-      bookColumns[0].appendChild(tr);
-      var th = document.createElement("th");
-      booksList[0].appendChild(th);
-      th.innerText = data.items[i].volumeInfo.title;
-      renderWiki(data.items[i].volumeInfo.title);
-      }})
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function(data){ 
+         console.log(data);
+         for (var i=0; i < data.items.length; i++){
+          var tr = document.createElement("tr");
+          // bookColumns[0].appendChild(tr);
+          var title = document.createElement("td");
+          title.textContent = data.items[i].volumeInfo.title;
+          var author = document.createElement("td");
+          author.textContent = data.items[i].volumeInfo.authors;
+          var info = document.createElement("td");
+          info.textContent = data.items[i].volumeInfo.info;
+          // booksList[0].appendChild(td);
+          // th.innerText = data.items[i].volumeInfo.title;
+          tr.appendChild(title);
+          tr.appendChild(author);
+          tr.appendChild(info);
+          table.appendChild(tr);
+         } 
+      });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
     .catch(function (error) {
-      console.log(error)
+      alert('Unable to connect to Google Books');
     });
 };
-
-function renderWiki(pages) {
-  var wikiSearch = "https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&list=search&formatversion=2&srsearch=" + pages;
-
-  fetch(wikiSearch)
-      .then(function(response){
-          return response.json();})
-      .then(function(responseData) {
-          console.log(responseData);
-      })
-      .catch(function(error){console.log(error);});
-}
 
